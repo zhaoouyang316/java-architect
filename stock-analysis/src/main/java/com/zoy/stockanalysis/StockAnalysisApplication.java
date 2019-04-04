543a463879ebbbbe65162f0cd511cbc13c428a48
zhaoouyang <zhaoouyang@qq.com>
2019-04-04 20:19:46 +0800

    feat: 初始化数据源，jpa引入

---
+++ stock-analysis/src/main/java/com/zoy/stockanalysis/StockAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.zoy.stockanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalysisApplication.class, args);
+	}
+
+}
