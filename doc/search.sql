e60fd6121baad1415fd7a9bf0bb868978e2df363
zhaoouyang@qq.com <zhaoouyang@qq.com>
2019-04-19 00:33:38 +0800

    feat: search sql

--- doc/search.sql
+++ doc/search.sql
@@ -15,3 +15,25 @@ settlement_price 结算价,
 volatility_percentage 波动百分比,
 broader_market_status  大盘涨跌
 from t_stock_analysis_record where  stock_analysis_id='168674780231237632'
+
+
+-- 查询逆向投资股票
+
+select * from t_stock_analysis where stock_name like '%逆向投资%'
+
+
+-- 查询逆向投资股票记录
+
+select
+tspr.spot_price 买入价格,
+tspr.stock_name 策略名称,
+tspr.stock_code 股票代码,
+tspr.position_number 持仓数量,
+tspr.yesterday_settlement 上期盈亏,
+tspr.volatility_percentage 波动幅度
+ from t_stock_price_record tspr
+ where stock_analysis_id='170889798196658176'
+
+ -- 查看逆向投资的盈亏
+
+ select * from t_stock_analysis_record where stock_analysis_id='170539967515197440'
